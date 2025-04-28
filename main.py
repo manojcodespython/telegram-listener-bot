8716054a0645140229e9277566ac6d89011a196b
manojcodespython <khatri.manoj35@gmail.com>
2025-04-29 00:52:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+import asyncio
 from telethon import TelegramClient, events
 
-api_id = 10705683
-api_hash = '7844949a3031003987ea51e99177ad70'
+api_id = 25521514  # Your API ID
+api_hash = '78e89cfc0f1c8b9d25a1a7c2b6c0b07b'  # Your API HASH
 
-client = TelegramClient('listener_session', api_id, api_hash)
+session_name = 'forwarder_session'  # Your .session filename without .session extension
+client = TelegramClient(session_name, api_id, api_hash)
 
-@client.on(events.NewMessage())
-async def handler(event):
-    if event.is_channel:
-        sender = await event.get_sender()
-        print(f"📢 Channel Name: {sender.title}")
-        print(f"🆔 Channel ID: {event.chat_id}")
+async def main():
+    @client.on(events.NewMessage)
+    async def handler(event):
+        try:
+            sender = await event.get_sender()
+            if event.chat and event.chat.title:
+                print(f"🔵 Channel Name: {event.chat.title}")
+                print(f"🆔 Channel ID: {event.chat_id}\n")
+        except Exception as e:
+            print(f"Error: {e}")
 
-client.start()
-print("✅ Listener Bot is running... Waiting for messages.")
-client.run_until_disconnected()
+    print("✅ Listener Bot started successfully. Waiting for messages...")
+    await client.run_until_disconnected()
+
+with client:
+    client.loop.run_until_complete(main())
